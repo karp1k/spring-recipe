91c3dd87c84181d5a1cb2b1bcc7e58a63001e58e
Tom <desktop-vm-ubuntu>
2019-12-28 14:56:43 +0300

    Index page

---
+++ src/main/java/guru/springframework/springrecipe/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package guru.springframework.springrecipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author kas
+ */
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/", "/index"})
+    public String getIndexPage() {
+        return "index";
+    }
+}
---
+++ src/test/java/guru/springframework/springrecipe/SpringRecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springrecipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
