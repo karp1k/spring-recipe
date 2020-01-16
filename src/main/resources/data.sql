6d425bba49671a4e3d8b4c47fbeb256fd003595d
Tom <desktop-vm-ubuntu>
2020-01-16 14:53:35 +0300

    init data for category and unit_of_measure tables

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO category (description) values ('american');
+INSERT INTO category (description) values ('italian');
+INSERT INTO category (description) values ('mexican');
+INSERT INTO category (description) values ('fast food');
+INSERT INTO unit_of_measure (description) values ('teaspoon');
+INSERT INTO unit_of_measure (description) values ('tablespoon');
+INSERT INTO unit_of_measure (description) values ('cup');
+INSERT INTO unit_of_measure (description) values ('pinch');
+INSERT INTO unit_of_measure (description) values ('ounce');
