85dd34401b70484117e2d9d7f7c6b88dd8812cb8
Tom <desktop-vm-ubuntu>
2020-01-20 19:16:40 +0300

    mockMvc unit test

--- src/test/java/guru/springframework/springrecipe/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/springrecipe/controllers/IndexControllerTest.java
@@ -8,6 +8,11 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
@@ -29,6 +34,19 @@ public class IndexControllerTest {
         MockitoAnnotations.initMocks(this);
         indexController = new IndexController(recipeService);
 
+    }
+
+    @Test
+    public void testMockMvc() {
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+
+        try {
+            mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                    .andExpect(MockMvcResultMatchers.status().isOk())
+                    .andExpect(MockMvcResultMatchers.view().name("index"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
