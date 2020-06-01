9a5e200f3b17f209d7757ad8b712c8efb787ff0a
Tom <desktop-vm-ubuntu>
2020-06-01 17:43:22 +0300

    add recipe creation form with rest paths

--- src/main/java/guru/springframework/springrecipe/controllers/RecipeController.java
+++ src/main/java/guru/springframework/springrecipe/controllers/RecipeController.java
@@ -1,11 +1,10 @@
 package guru.springframework.springrecipe.controllers;
 
+import guru.springframework.springrecipe.commands.RecipeCommand;
 import guru.springframework.springrecipe.services.RecipeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author kas
@@ -24,5 +23,18 @@ public class RecipeController {
     public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
         return "recipe/show";
+    }
+
+    @GetMapping("/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+        return "recipe/recipe-form";
+
+    }
+
+    @PostMapping
+    public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+        RecipeCommand saveCommand = recipeService.saveRecipeCommand(command);
+        return "redirect:/recipe/show/" + saveCommand.getId();
     }
 }
